9528c1a80298a90c155bf2f22e3d84f53d56c822
M AKIF <makifhussainmn@gmail.com>
2023-02-13 23:41:50 +0500

    mn brands

---
+++ app.js
@@ -0,0 +1,138 @@
+// var a =10;
+// var b=20;
+// var c =(a+b)
+// alert(c)
+
+//  var a =10;
+// var b= 50;
+// var c=(a+b);
+// console.log(c)
+ 
+
+
+// var b = +prompt( "when is your age","your age is");
+//  var a =10;
+//  var c=b+a;
+// console.log("The out is" +""+c)
+// var c =document.write ("what is your Value","your value is")
+// alert(c) 
+
+
+// var b = +prompt( "when is your age","your age is");
+//  var a =10;
+//  var c=b+a;
+// decument.write("The out is" +""+c)
+
+// var age =+prompt("what is your age");
+
+// if(age==16){
+//     alert("your age is equal")
+// } else if(age < 16 ){
+// alert("you age is less")
+
+
+// }
+// else{
+// alert("you are no more in this")
+
+
+// }
+
+
+// var age =+prompt("what is your age");
+
+// var gander=prompt("Enter your Gander");
+
+// if( age < 14 && gander== "male")
+// {
+// alert("You are allow to ride")
+// }
+// else {
+// alert("you are not allow in ride")
+
+// }
+
+
+
+// var a = +prompt("write the nummber") ;
+// 
+
+
+
+// var x = 5 
+// ; var y = 10
+// ;var a =30;
+// var b = 15;
+ 
+//  if(x+y === a-b) {
+// alert ("Correct" )
+//  } 
+//  else{
+//     alert(incorrect )
+//  }
+
+
+// var a = 10;
+// if ( a !== 15;){
+//     document.write("Correct")
+// }
+// else{
+//     document.write( "Incorrect")
+// }
+
+
+
+
+
+
+
+
+
+
+
+// var percent = +prompt("Enter your percent")
+//  if (percent >=80 &&  percent <=100 )
+// {
+//     alert("graduate is A+")
+// }
+
+// else if (percent >=80 && percent <=90 )
+// {
+//     alert("graduate is A")
+// }
+
+// else if (percent >=70 && percent <=80 )
+// {
+//     alert("graduate is B")
+// }
+
+
+// else if (percent >=60 && percent <=70 )
+// {
+//     alert("graduate is C")
+// }
+
+// else if (percent >=50 && percent <=60 )
+// {
+//     alert("graduate is D")
+// }
+
+// else if (percent >=40 && percent <=50 )
+// {
+//     alert("graduate is E")
+// }
+
+// else if (percent >=0 && percent <=30 )
+// {
+//     alert("graduate is F")
+// }
+// else { 
+//     alert("You are enter incorrect percent")
+// }
+
+
+
+
+var students = ["ali","akif","arslan"]
+
+alert( "wellcome"+students[1])
